f84d3c2e1487259446de0c39c576db72204fdb17
Yoann Gern <yoann@yoanngern.ch>
2017-02-18 09:49:22 +0100

    update google analytics

--- wp-content/themes/esbs_2017/header.php
+++ wp-content/themes/esbs_2017/header.php
@@ -2,10 +2,10 @@
 <html <?php language_attributes(); ?> class="no-js">
 <head>
 
-	<title><?php echo get_bloginfo( 'title'); ?></title>
+	<title><?php echo get_bloginfo( 'title' ); ?></title>
 
 
-	<meta name="description" content="<?php echo get_bloginfo( 'description'); ?>">
+	<meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
 
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 
@@ -43,12 +43,19 @@
 
 
 	<script>
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+		(function (i, s, o, g, r, a, m) {
+			i['GoogleAnalyticsObject'] = r;
+			i[r] = i[r] || function () {
+					(i[r].q = i[r].q || []).push(arguments)
+				}, i[r].l = 1 * new Date();
+			a = s.createElement(o),
+				m = s.getElementsByTagName(o)[0];
+			a.async = 1;
+			a.src = g;
+			m.parentNode.insertBefore(a, m)
+		})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-		ga('create', 'UA-36835114-5', 'auto');
+		ga('create', 'UA-92172259-1', 'auto');
 		ga('send', 'pageview');
 
 	</script>
