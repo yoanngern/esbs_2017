6bc7c3a42379ab9e80f023834a682d6f7ab2569e
Yoann Gern <yoann@yoanngern.ch>
2017-03-15 11:26:16 +0100

    update footer

--- wp-content/themes/esbs_2017/footer.php
+++ wp-content/themes/esbs_2017/footer.php
@@ -3,11 +3,15 @@
 <footer>
 
 
-	<a href="/" id="logo_footer"></a>
+    <a href="/" id="logo_footer"></a>
 
-	<p><em>ESBS Foundation</em><br/>
-		Route du Flon 28, 1610 Oron, Switzerland<br/>
-		<a href="mailto:contact@esbs.org">contact@esbs.org</a></p>
+    <p><em>ESBS Foundation</em><br/>
+        Route du Flon 28, 1610 Oron, Switzerland<br/>
+        <a href="mailto:contact@esbs.org">contact@esbs.org</a></p>
+
+    <p><em>To Give</em><br/>
+        IBAN: CH340024324320952701Z<br/>
+        BIC: UBSWCHZH80A</p>
 
 
 </footer>
