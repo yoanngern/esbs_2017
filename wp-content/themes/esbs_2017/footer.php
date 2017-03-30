35f012d8becf022f7fa07a77c0a72e8c45eac1eb
Yoann Gern <yoann@yoanngern.ch>
2017-03-30 18:17:10 +0200

    update footer

--- wp-content/themes/esbs_2017/footer.php
+++ wp-content/themes/esbs_2017/footer.php
@@ -3,7 +3,7 @@
 <footer>
 
 
-    <a href="/" id="logo_footer"></a>
+    <a href="<?php echo pll_home_url(); ?>" id="logo_footer"></a>
 
     <p><em><?php echo pll_e('ESBS Foundation'); ?></em><br/>
         <?php echo pll_e('Route du Flon 28, 1610 Oron, Switzerland'); ?><br/>
