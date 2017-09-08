cb9af0174087539521c8b9afa7e5c4c182a591f3
Yoann Gern <yoann@yoanngern.ch>
2017-09-08 08:15:14 +0200

    update join

--- wp-content/themes/esbs_2017/page-custom.php
+++ wp-content/themes/esbs_2017/page-custom.php
@@ -5,7 +5,7 @@ get_header();
 global $post;
 $post_slug = $post->post_name;
 
-if ( $post_slug == 'join-2' || $post_slug == 'join' ) {
+if ( $post_slug == 'join-2' || $post_slug == 'join' || $post_slug == 'join-a' || $post_slug == 'join-b' || $post_slug == 'join-c' ) {
 	get_template_part( 'template-parts/custom/join' );
 }
 
