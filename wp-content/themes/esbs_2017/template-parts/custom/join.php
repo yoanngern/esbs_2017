cdbf1b85ea2db036fa1e0b8bea4598b57b94df6c
Yoann Gern <yoann@yoanngern.ch>
2017-11-20 16:11:00 +0100

    update join page

--- wp-content/themes/esbs_2017/template-parts/custom/join.php
+++ wp-content/themes/esbs_2017/template-parts/custom/join.php
@@ -5,7 +5,8 @@
 
         <a href="#" class="close"></a>
 
-        <script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/youtube_player.min.js"></script>
+        <script type="text/javascript"
+                src="<?php echo get_stylesheet_directory_uri(); ?>/js/youtube_player.min.js"></script>
 
         <div id="player"></div>
     </div>
@@ -13,28 +14,28 @@
 
     <div class="bg_black"></div>
 
-	<article class="center">
+    <article class="center">
 
-		<div id="logo"></div>
+        <div id="logo"></div>
 
-		<h1>Join the movement today</h1>
-		<h2>One movement of people from all backgrounds to see the Gospel of Jesus Christ change Europe</h2>
+        <h1><?php echo pll_e( 'Join the movement today' ); ?>Join the movement today</h1>
+        <h2><?php echo pll_e( 'One movement of people from all backgrounds to see the Gospel of Jesus Christ change Europe' ); ?></h2>
 
         <a href="#" class="play"></a>
 
-		<?php echo do_shortcode('[mc4wp_form id="1337"]'); ?>
+		<?php echo do_shortcode( '[mc4wp_form id="1337"]' ); ?>
 
 		<?php //echo do_shortcode('[mc4wp_form id="1255"]'); ?>
 
-	</article>
+    </article>
 
-    <a href="<?php echo pll_home_url(); ?>" class="home">Tell me more</a>
+    <a href="<?php echo pll_home_url(); ?>" class="home"><?php echo pll_e( 'Tell me more' ); ?></a>
 
-	<div class="video">
-		<video id="bgvid" autoplay loop>
-			<source src="https://player.vimeo.com/external/231915594.sd.mp4?s=7cba34b8cf0ac668416224e2bed073cff61931d0&profile_id=165"
-			        type="video/mp4">
-		</video>
-	</div>
+    <div class="video">
+        <video id="bgvid" autoplay loop muted plays-inline>
+            <source src="https://player.vimeo.com/external/231915594.sd.mp4?s=7cba34b8cf0ac668416224e2bed073cff61931d0&profile_id=165"
+                    type="video/mp4">
+        </video>
+    </div>
 
 </section>
