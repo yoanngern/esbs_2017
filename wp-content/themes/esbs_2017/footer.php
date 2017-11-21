0054e852a96836e049c34a06fc2744d2757043ae
Yoann Gern <yoann@yoanngern.ch>
2017-11-21 12:11:56 +0100

    update custome page

--- wp-content/themes/esbs_2017/footer.php
+++ wp-content/themes/esbs_2017/footer.php
@@ -7,12 +7,15 @@
 
     <a href="<?php echo pll_home_url(); ?>" id="logo_footer"></a>
 
-    <a href="<?php echo pll_home_url(); ?>donation" class="button"><?php pll_e('Donation') ?></a>
+
 
     <p><em><?php echo pll_e('ESBS Foundation'); ?></em><br/>
         <?php echo pll_e('Route du Flon 28, 1610 Oron, Switzerland'); ?><br/>
         <a href="mailto:contact@esbs.org">contact@esbs.org</a></p>
 
+    <p class="social"><em><?php echo pll_e('Follow on'); ?></em><br/>
+        <a href="https://www.facebook.com/EuropeShallBeSaved/">Facebook</a>|<a href="https://twitter.com/ESBSmovement">Twitter</a>|<a href="https://www.instagram.com/europeshallbesaved/">Instagram</a>
+    </p>
 
 </footer>
 
