7275c7fc9fe4eb5f85fab9c4e2538a0372799ff0
Yoann Gern <yoann@yoanngern.ch>
2018-02-22 16:40:41 +0100

    update facebook options

--- wp-content/themes/esbs_2017/template-parts/divers/facebook_login.php
+++ wp-content/themes/esbs_2017/template-parts/divers/facebook_login.php
@@ -4,7 +4,7 @@
             appId: <?php echo get_field('facebook_app_id', 'option'); ?>,
             cookie: true,
             xfbml: true,
-            version: <?php echo get_field('facebook_app_version', 'option'); ?>
+            version: <?php echo get_field('facebook_api_version', 'option'); ?>
         });
 
         FB.AppEvents.logPageView();
