ae3c6a176ea4758b5347f79c8de6e491d1959850
Yoann Gern <yoann@yoanngern.ch>
2018-02-22 16:41:09 +0100

    update facebook options

--- wp-content/themes/esbs_2017/template-parts/divers/facebook_login.php
+++ wp-content/themes/esbs_2017/template-parts/divers/facebook_login.php
@@ -1,10 +1,10 @@
 <script>
     window.fbAsyncInit = function () {
         FB.init({
-            appId: <?php echo get_field('facebook_app_id', 'option'); ?>,
+            appId: '<?php echo get_field( 'facebook_app_id', 'option' ); ?>',
             cookie: true,
             xfbml: true,
-            version: <?php echo get_field('facebook_api_version', 'option'); ?>
+            version: '<?php echo get_field( 'facebook_api_version', 'option' ); ?>'
         });
 
         FB.AppEvents.logPageView();
